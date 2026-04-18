6018ab8405741f199af3cd9ef6657f536e4368a8
Tao Jin <taojin83@gmail.com>
2026-04-18 19:37:15 -0400

    1. When calculate the strain energy threshold ft^2/E, use E in the general 3D case. 2. Make sure the phasefield solutions are feasible after the line search step.

--- main.cc
+++ main.cc
@@ -1131,16 +1131,10 @@ namespace PhaseField
 		   const std::string & phasefield_name,
 		   const bool   plane_stress_flag)
     {
-      double E0 = lame_mu * (3*lame_lambda + 2*lame_mu) / (lame_lambda + lame_mu);
+      // For the equivalent of 1D strain energy at fracture ft^2/(2E)
+      // the Young's modulus E is for 3D case
+      const double E0 = lame_mu * (3*lame_lambda + 2*lame_mu) / (lame_lambda + lame_mu);
       const double phasefield_geo_constant = phasefield_coefficient_constant(phasefield_name);
-
-      // 2D plane stress case
-      if (    dim == 2
-  	   && plane_stress_flag)
-        {
-          double my_lambda = 2 * lame_mu * lame_lambda / (lame_lambda + 2 * lame_mu);
-          E0 = lame_mu * (3*my_lambda + 2*lame_mu) / (my_lambda + lame_mu);
-        }
 
       double a1 = 0.0;
       if (phasefield_name == "PFCZM")
@@ -1338,6 +1332,7 @@ namespace PhaseField
     // m_active_set_phasefield has 0 (inactive constraint)
     //                          or 1 (active constraint lower bound)
     //                          or 2 (active constraint upper bound)
+    //                          or 3 (active constraint lower/upper bound)
     // In order to add active set into the VTK output, we have to declare
     // it as double, not int or unsigned int
     Vector<double> m_active_set_phasefield;
@@ -1399,6 +1394,7 @@ namespace PhaseField
     void make_grid_case_10();
     void make_grid_case_11();
     void make_grid_case_12();
+    void make_grid_case_13();
 
     void setup_system();
 
@@ -1761,9 +1757,9 @@ namespace PhaseField
           {
             trial_solution = solution_phasefield_total(i) - m_system_rhs.block(m_d_dof)[i];
 
-            if (trial_solution < m_solution.block(m_d_dof)[i])
+            if (trial_solution <= m_solution.block(m_d_dof)[i])
               error_res.block(m_d_dof)[i] = m_solution.block(m_d_dof)[i] - solution_phasefield_total(i);
-            else if (trial_solution > upper_limit)
+            else if (trial_solution >= upper_limit)
               error_res.block(m_d_dof)[i] = upper_limit - solution_phasefield_total(i);
             else
               error_res.block(m_d_dof)[i] = (-m_system_rhs.block(m_d_dof)[i]);
@@ -1835,7 +1831,7 @@ namespace PhaseField
             Assert( (poisson_ratio <= 0.5)&(poisson_ratio >=-1.0) , ExcInternalError());
 
             const double c_alpha = phasefield_coefficient_constant(m_parameters.m_phasefield_name);
-	    double E0 = lame_mu * (3*lame_lambda + 2*lame_mu) / (lame_lambda + lame_mu);
+	    const double E0 = lame_mu * (3*lame_lambda + 2*lame_mu) / (lame_lambda + lame_mu);
 
             m_logfile << "\tRegion " << material_region << " : " << std::endl;
             m_logfile << "\t\tLame lambda = " << lame_lambda << std::endl;
@@ -1856,14 +1852,6 @@ namespace PhaseField
             m_logfile << "\t\ta3 (the coefficient of the a1*a3*d^3 term\n"
                          "\t\t\tin the denominator of the degradation function) = "
         	      << a3 << std::endl;
-
-            // 2D plane stress case
-            if (    dim == 2
-        	 && m_parameters.m_plane_stress)
-              {
-                double my_lambda = 2 * lame_mu * lame_lambda / (lame_lambda + 2 * lame_mu);
-                E0 = lame_mu * (3*my_lambda + 2*lame_mu) / (my_lambda + lame_mu);
-              }
 
             if (m_parameters.m_phasefield_name == "AT2")
               {
@@ -1956,8 +1944,8 @@ namespace PhaseField
             m_logfile << "Material data file has " << m_material_data.size() << " rows. However, "
         	      << "the mesh has " << total_material_regions << " material regions."
 		      << std::endl;
-            Assert(m_material_data.size() == total_material_regions,
-                       ExcDimensionMismatch(m_material_data.size(), total_material_regions));
+            AssertThrow(m_material_data.size() == total_material_regions,
+                        ExcDimensionMismatch(m_material_data.size(), total_material_regions));
           }
         myfile.close();
       }
@@ -2488,6 +2476,8 @@ namespace PhaseField
       make_grid_case_11();
     else if (m_parameters.m_scenario == 12)
       make_grid_case_12();
+    else if (m_parameters.m_scenario == 13)
+      make_grid_case_13();
     else
       Assert(false, ExcMessage("The scenario has not been implemented!"));
 
@@ -3700,7 +3690,7 @@ namespace PhaseField
     for (unsigned int i = 0; i < 80; ++i)
       m_logfile << "*";
     m_logfile << std::endl;
-    m_logfile << "\t\t\t1-D bar (structured)" << std::endl;
+    m_logfile << "\t\t\t1-D bar (transversely stress free)" << std::endl;
     for (unsigned int i = 0; i < 80; ++i)
       m_logfile << "*";
     m_logfile << std::endl;
@@ -3737,6 +3727,73 @@ namespace PhaseField
 		face->set_boundary_id(4);
 	    }
 	}
+  }
+
+  template <int dim>
+  void PhaseFieldMonolithicSolve<dim>::make_grid_case_13()
+  {
+    for (unsigned int i = 0; i < 80; ++i)
+      m_logfile << "*";
+    m_logfile << std::endl;
+    m_logfile << "\t\t\t1-D bar with defect" << std::endl;
+    for (unsigned int i = 0; i < 80; ++i)
+      m_logfile << "*";
+    m_logfile << std::endl;
+
+    AssertThrow(dim==2, ExcMessage("The dimension has to be 2D!"));
+
+    double const length = 200.0;
+    double const width = 1.0;
+    double const h_size = 0.1;
+
+    std::vector<unsigned int> repetitions(dim, 1);
+    repetitions[0] = length / h_size;
+    repetitions[1] = width  / h_size;
+
+    Triangulation<dim> tmp_triangulation;
+
+    GridGenerator::subdivided_hyper_rectangle(tmp_triangulation,
+				     	      repetitions,
+					      Point<dim>( 0.0,      0.0 ),
+					      Point<dim>( length,   width ) );
+
+    std::set<typename Triangulation< dim >::active_cell_iterator >
+      cells_to_remove;
+
+    for (const auto &cell : tmp_triangulation.active_cell_iterators())
+      {
+        if (   std::fabs(cell->center()[0] - length/2) < h_size
+            && (   cell->center()[1] < h_size
+        	|| cell->center()[1] > width - h_size)
+	    )
+	  {
+	    cells_to_remove.insert(cell);
+	  }
+      }
+
+    GridGenerator::create_triangulation_with_removed_cells(tmp_triangulation,
+							   cells_to_remove,
+							   m_triangulation);
+
+    for (const auto &cell : m_triangulation.active_cell_iterators())
+      {
+	for (const auto &face : cell->face_iterators())
+	  {
+	    if (face->at_boundary() == true)
+	      {
+		if ( (std::fabs(face->center()[0] - 0.0 ) < 1.0e-9) )
+		  face->set_boundary_id(0);
+		else if ( (std::fabs(face->center()[0] - length ) < 1.0e-9) )
+		  face->set_boundary_id(1);
+		else if ( (std::fabs(face->center()[1] - 0.0 ) < 1.0e-9) )
+		  face->set_boundary_id(2);
+		else if ( (std::fabs(face->center()[1] - width ) < 1.0e-9) )
+		  face->set_boundary_id(3);
+		else
+		  face->set_boundary_id(4);
+	      }
+	  }
+      }
   }
 
   template <int dim>
@@ -4076,6 +4133,64 @@ namespace PhaseField
 	      }
 	  }
 	else if (m_parameters.m_scenario == 12)
+	  {
+	    // Dirichlet B.C. left surface (x = 0)
+	    const int boundary_id_left_surface = 0;
+	    VectorTools::interpolate_boundary_values(m_dof_handler,
+						     boundary_id_left_surface,
+						     Functions::ZeroFunction<dim>(m_n_components),
+						     m_constraints,
+						     m_fe.component_mask(x_displacement));
+	    VectorTools::interpolate_boundary_values(m_dof_handler,
+						     boundary_id_left_surface,
+						     Functions::ZeroFunction<dim>(m_n_components),
+						     m_constraints,
+						     m_fe.component_mask(phasefield));
+
+	    const int boundary_id_right_surface = 1;
+	    VectorTools::interpolate_boundary_values(m_dof_handler,
+						     boundary_id_right_surface,
+						     Functions::ZeroFunction<dim>(m_n_components),
+						     m_constraints,
+						     m_fe.component_mask(phasefield));
+
+	    const double time_inc = m_time.get_delta_t();
+	    double disp_magnitude = m_time.get_magnitude();
+	    VectorTools::interpolate_boundary_values(m_dof_handler,
+						     boundary_id_right_surface,
+						     Functions::ConstantFunction<dim>(
+						       disp_magnitude*time_inc, m_n_components),
+						     m_constraints,
+						     m_fe.component_mask(x_displacement));
+
+	    typename Triangulation<dim>::active_vertex_iterator vertex_itr;
+	    vertex_itr = m_triangulation.begin_active_vertex();
+	    std::vector<types::global_dof_index> node_leftbottom(m_fe.dofs_per_vertex);
+	    std::vector<types::global_dof_index> node_rightbottom(m_fe.dofs_per_vertex);
+
+	    for (; vertex_itr != m_triangulation.end_vertex(); ++vertex_itr)
+	      {
+		if (   (std::fabs(vertex_itr->vertex()[0] - 0.0) < 1.0e-9)
+		    && (std::fabs(vertex_itr->vertex()[1] - 0.0) < 1.0e-9) )
+		  {
+		    node_leftbottom = usr_utilities::get_vertex_dofs(vertex_itr, m_dof_handler);
+		  }
+		if (   (std::fabs(vertex_itr->vertex()[0] - 200.0) < 1.0e-9)
+		    && (std::fabs(vertex_itr->vertex()[1] - 0.0) < 1.0e-9) )
+		  {
+		    node_rightbottom = usr_utilities::get_vertex_dofs(vertex_itr, m_dof_handler);
+		  }
+	      }
+	    m_constraints.add_line(node_leftbottom[0]);
+	    m_constraints.set_inhomogeneity(node_leftbottom[0], 0.0);
+
+	    m_constraints.add_line(node_leftbottom[1]);
+	    m_constraints.set_inhomogeneity(node_leftbottom[1], 0.0);
+
+	    m_constraints.add_line(node_rightbottom[1]);
+	    m_constraints.set_inhomogeneity(node_rightbottom[1], 0.0);
+	  }
+	else if (m_parameters.m_scenario == 13)
 	  {
 	    // Dirichlet B.C. left surface (x = 0)
 	    const int boundary_id_left_surface = 0;
@@ -4671,8 +4786,6 @@ namespace PhaseField
 
     m_timer.leave_subsection();
   }
-
-
 
   template <int dim>
   double PhaseFieldMonolithicSolve<dim>::line_search_stepsize_gradient_based(const BlockVector<double> & BFGS_p_vector,
@@ -5486,7 +5599,7 @@ namespace PhaseField
 	// inactive phasefield dof
 	if (m_active_set_phasefield(i) < 0.5)
 	  solution_delta_cauchy_point.block(m_d_dof)[i] = solution_delta.block(m_d_dof)[i]
-						+ t_old * gradient_d.block(m_d_dof)[i];
+					                + t_old * gradient_d.block(m_d_dof)[i];
       }
 
     // There are no active constraints in the displacement field
@@ -6194,9 +6307,9 @@ namespace PhaseField
 	alpha_backtrack = 1.0;
 	search_direction *= alpha_backtrack;
 
-	LBFGS_update.block(m_u_dof) = solution_delta_cauchy_point.block(m_u_dof);
-	LBFGS_update.block(m_u_dof) += search_direction.block(m_u_dof);
-	LBFGS_update.block(m_u_dof) -= solution_delta.block(m_u_dof);
+	LBFGS_update.block(m_u_dof) = solution_delta_cauchy_point.block(m_u_dof)
+	                            + search_direction.block(m_u_dof)
+	                            - solution_delta.block(m_u_dof);
 
 	for (unsigned int i = 0; i < m_dofs_per_block[m_d_dof]; ++i)
 	  {
@@ -6209,7 +6322,7 @@ namespace PhaseField
 					     + search_direction.block(m_d_dof)[i]
 					     - solution_delta.block(m_d_dof)[i];
 	  }
-
+/*
 	// make sure the phasefield solutions are feasible
 	for(unsigned int i = 0; i < m_dofs_per_block[m_d_dof]; ++i)
 	  {
@@ -6222,7 +6335,7 @@ namespace PhaseField
 	      LBFGS_update.block(m_d_dof)[i] = 1.0 - m_solution.block(m_d_dof)[i]
 						   - solution_delta.block(m_d_dof)[i];
 	  }
-
+*/
 	m_constraints.distribute(LBFGS_update);
 
 	// We need a line search algorithm to decide line_search_parameter
@@ -6249,6 +6362,21 @@ namespace PhaseField
           }
 
 	LBFGS_update *= line_search_parameter;
+
+	// make sure the phasefield solutions are feasible
+	for(unsigned int i = 0; i < m_dofs_per_block[m_d_dof]; ++i)
+	  {
+	    if (solution_delta.block(m_d_dof)[i] + LBFGS_update.block(m_d_dof)[i] < 0.0)
+	      LBFGS_update.block(m_d_dof)[i] = -solution_delta.block(m_d_dof)[i];
+
+	    if (  solution_delta.block(m_d_dof)[i]
+		+ m_solution.block(m_d_dof)[i]
+		+ LBFGS_update.block(m_d_dof)[i] > 1.0)
+	      LBFGS_update.block(m_d_dof)[i] = 1.0 - m_solution.block(m_d_dof)[i]
+						   - solution_delta.block(m_d_dof)[i];
+	  }
+
+	m_constraints.distribute(LBFGS_update);
 
         get_error_update(LBFGS_update, m_error_update);
         if (LBFGS_iteration == 1)
